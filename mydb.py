48197650a770b4a5ed460a8dbd441fb27dd65c5a
TwooPeeks <ionetecosminmihai@yahoo.com>
2023-10-24 20:31:08 +0300

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host ='localhost',
+    user = 'root',
+    passwd = 'password123',
+
+
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute('CREATE DATABASE CRMProj')
+
+print('All Done!')
